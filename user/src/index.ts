9006548e29cca06f4deda82d455598fb9d496304
Vinayak <vinayakmaheshwari57@gmail.com>
2026-06-18 18:31:07 +0530

    reddis errror handling added

--- user/src/index.ts
+++ user/src/index.ts
@@ -16,6 +16,10 @@ export const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
 
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error:", err);
+});
+
 redisClient
   .connect()
   .then(() => console.log("connected to redis"))
